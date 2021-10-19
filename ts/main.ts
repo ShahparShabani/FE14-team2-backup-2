0e1c4c1206f43754ef228981455f45f613c72b20
shahparshabani <shahpar.shabani@g-experts.net>
2021-10-19 22:10:22 +0200

    Fix bug of Carousel height

--- ts/main.ts
+++ ts/main.ts
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    // Listen to scroll event to fix navbar position to top and change its style.
     window.addEventListener('scroll', function () {
         if (window.scrollY > 78) {
             console.log('scroll > 78');
-            (document.getElementById('navbar_top') as HTMLElement).classList.remove('c-navbar');
-            (document.getElementById('navbar_top') as HTMLElement).classList.add('c-fixed-top');
+
+            // Remove navbar from carousel and position it to top
+            (document.getElementById('navbar_top') as HTMLElement).classList.remove('c-navbar', 'navbar-dark', 'bg-dark');
+            (document.getElementById('navbar_top') as HTMLElement).classList.add('c-fixed-top', 'shadow', 'navbar-light', 'bg-light');
 
         } else {
             console.log('scroll < 78');
-            (document.getElementById('navbar_top') as HTMLElement).classList.remove('c-fixed-top');
-            (document.getElementById('navbar_top') as HTMLElement).classList.add('c-navbar');
 
-            // remove padding top from body
+            // Position navbar over the carousel again
+            (document.getElementById('navbar_top') as HTMLElement).classList.remove('c-fixed-top', 'shadow', 'navbar-light', 'bg-light');
+            (document.getElementById('navbar_top') as HTMLElement).classList.add('c-navbar', 'navbar-dark', 'bg-dark');
+
+            // Remove padding top from body
             document.body.style.paddingTop = '0';
         }
     });
